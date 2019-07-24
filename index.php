64fb01e1b76aa4c6d4c74876d11d21b298a025ee
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-24 23:10:54 +0900

    commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $url = parse_url(getenv("mysql://b1f581c6e88461:77723a9e@us-cdbr-iron-east-02.cleardb.net/heroku_82cd1d27322e58c?reconnect=true"));
 
-    $server = $url["host"];
+    $server = $url["127.0.0.1"];
     $username = $url["b1f581c6e88461"];
     $password = $url["77723a9e"];
     $db = substr($url["heroku_82cd1d27322e58c"], 1);
