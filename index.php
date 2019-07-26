dc045f6c47113eb790bdbb1fed73ae0dab80cf42
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 21:53:43 +0900

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
     <?php
       $db = new PDO("mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_82cd1d27322e58c","b1f581c6e88461","77723a9e");
       $ps = $db->query("SELECT * FROM products");
+      $r = $ps->fetch();
       print "~~~~~~~~~~fin~~~~~~";
     ?>
   </body>
