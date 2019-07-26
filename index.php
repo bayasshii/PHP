a86aee87626c8382c453210f3f8948e32045e84d
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 21:17:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>送信</title>
   </head>
   <body>
-    <p>何か喋って</p>
+    <p>何か喋ってよ</p>
     <form action="sns_1.php" method="post">
       <p>名前</p>
       <input type="text" name="name">
@@ -17,9 +17,10 @@
     </form>
 
     <?php
+
       $db = new PDO("mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_82cd1d27322e58c","b1f581c6e88461","77723a9e");
 
-      $ps = $db->query(" SELECT * FROM products ");
+      $ps = $db->query("SELECT * FROM products");
 
       $r = $ps->fetch();
 
