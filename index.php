283109fa9cc20c3c50143f695071283893f2c46a
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 23:20:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
       $db = new PDO("mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_82cd1d27322e58c","b1f581c6e88461","77723a9e");
       $ps = $db->query("SELECT * FROM products");
       while ($r = $ps->fetch()){
-        print "{$r['id']} {$r['name']} {$r['price']}</hr>";
+        print "{$r['id']} {$r['name']} {$r['price']}<hr>";
       }
 
     ?>
