9a9f4d746eb3235ad97822887d064b3042a2a5e8
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 19:15:57 +0900

    Update uke.php

--- uke.php
+++ uke.php
@@ -10,7 +10,13 @@
     ?>
     <?php
       $db = new mysqli('us-cdbr-iron-east-02','b9e1f717777aa7:','d99be634','heroku_537e9386d49f912?reconnect=true');
-      print $db;
+      if ($db->connect_error) {
+        echo $db->connect_error;
+        exit ();
+      } else {
+        $db->set_charset ( "utf8" );
+        echo 'ok';
+      }
     ?>
     <?php
     phpinfo();
