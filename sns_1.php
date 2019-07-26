cbe46dae5fe1e2f734866defaf151a23f4809cdf
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 20:46:15 +0900

    commit

---
+++ sns_1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>受信</title>
+  </head>
+  <body>
+    <?php
+      print $_POST["my_text"];
+    ?>
+    <?php
+      $product_name = htmlspecialchars($_POST["name"], END_QUOTES);
+      $product_price = htmlspecialchars($_POST["price"], END_QUOTES);
+      $product_eplain = htmlspecialchars($_POST["eplain"], END_QUOTES);
+
+      $db = new mysqli('us-cdbr-iron-east-02.cleardb.net','b1f581c6e88461','77723a9e','heroku_82cd1d27322e58c');
+
+      $db->query("INSERT INTO td (name, price, eplain))
+                  VALUES (NULL, '$product_name', '$product_price', '$product_eplain')");
+      print "書き込みに成功しました！";
+      print "<p><a href="/">一覧表示へ</a></p>"
+    ?>
+
+  </body>
+</html>
--- uke.php
+++ uke.php
@@ -10,12 +10,19 @@
     ?>
     <?php
       $db = new mysqli('us-cdbr-iron-east-02.cleardb.net','b1f581c6e88461','77723a9e','heroku_82cd1d27322e58c');
+
       if ($db->connect_error) {
         echo $db->connect_error;
         exit ();
       } else {
         $db->set_charset ( "utf8" );
         echo 'ok';
+      }
+
+      $ps = $db->query(" SELECT * FROM tb ");
+
+      while ($r = $ps->fetch()){
+        print "{$r[]}"
       }
     ?>
     <?php
