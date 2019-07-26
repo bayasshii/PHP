35eb6b08a1a938c30fc31396c47bb5e3a895fd34
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 23:30:35 +0900

    Update sns_1.php

--- sns_1.php
+++ sns_1.php
@@ -14,7 +14,7 @@
 
       $db = new PDO("mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_82cd1d27322e58c","b1f581c6e88461","77723a9e");
 
-      $db->query("INSERT INTO products (id,name,price)) VALUES (10, '$product_name', '$product_price')");
+      $db->query("INSERT INTO products (id,name,price) VALUES (NULL, '$product_name', '$product_price')");
 
       print "書き込みに成功しました！";
       print "<p><a href='/'>一覧表示へ</a></p>"
