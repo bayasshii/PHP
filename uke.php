af0d5c3c0d939be51aab7223ac92b468af63edeb
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-26 19:14:30 +0900

    Update uke.php

--- uke.php
+++ uke.php
@@ -9,7 +9,7 @@
       print $_POST["my_text"];
     ?>
     <?php
-      $db = new mysqli('us-cdbr-iron-east-02','b9e1f717777aa7:','d99be634','heroku_537e9386d49f912');
+      $db = new mysqli('us-cdbr-iron-east-02','b9e1f717777aa7:','d99be634','heroku_537e9386d49f912?reconnect=true');
       print $db;
     ?>
     <?php
