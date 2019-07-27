7c45afde7eb8eb90a9d3cc308f3af6337429e471
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 00:14:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
       print "FirstHoge";
       $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
       print "SecondHoge";
-      $jsondata = $db->query("SELECT * FROM cakes ORDER BY details DESC");
+      $jsondatas = $db->query("SELECT * FROM cakes");
       print "ThirdHoge";
-      while ($r = $jsonData->fetchObject(){
+      while ($r = $jsonDatas['details']->fetch(){
         print "YonHoge";
-        $r = json_decode($jsonData);
+        $r = json_decode($jsonData, true);
         print "GoHoge";
         print "{$r['url']} {$r['price']} {$r['title']} {$r['explain']}<hr>";
       }
