c0908edb214d772111294ba8a1ece8e85d3c15f6
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 04:57:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                 </div>
                 <div class='cakeContents__right'>
                   <div class='cakeContents--url'><img src='https://treeethreee.s3-ap-northeast-1.amazonaws.com/ichigoFTHG4922_TP_V.jpg' alt='cakeImg'></div>
-                  <divclass='cakeContents--price'>{$r['price']}円</div>
+                  <div class='cakeContents--price'>{$r['price']}円</div>
                 </div>
               </div>
             </div>
