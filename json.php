9e689633535efb5186f27cd2d5407687bf1ead03
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-27 23:41:13 +0900

    Update json.php

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ require_once('config.php');
 require_once('functions.php');
 
 mb_language("uni");
-mb_internal_encoding("utf-8"); //内部文字コードを変更
+mb_internal_encoding("utf-8");
 mb_http_input("auto");
 mb_http_output("utf-8");
 
@@ -23,4 +23,4 @@ while($row = $sth->fetch(PDO::FETCH_ASSOC)){
 
 //jsonとして出力
 header('Content-type: application/json');
-echo json_encode($userData);
+print json_encode($cakeData);
