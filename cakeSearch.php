ef2b94b32c19cf4f0f9bf21556f49e09a321f783
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 03:01:43 +0900

    Update cakeSearch.php

--- cakeSearch.php
+++ cakeSearch.php
@@ -12,7 +12,7 @@
 
       $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
 
-      $ps = $db->query("SELECT * FROM cakes WHERE title like '%$cake_searchWord%' OR explain like '%$cake_searchWord%'");
+      $ps = $db->query("SELECT * FROM cakes WHERE title like '%$cake_searchWord%'");
 
       while ($r = $ps->fetch()){
         print "{$r['shop']} {$r['title']} {$r['price']} {$r['explanation']} {$r['url']}<hr>";
