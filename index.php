0868027b74696b464944ccc557cf1f22f41bae7e
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 03:34:20 +0900

    Update index.php

--- index.php
+++ index.php
@@ -51,18 +51,19 @@
     <?php
 
       $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
+
       $tb = $db->query("SELECT * FROM cakes");
 
       while ($r = $tb->fetch()){
         print "
-        <div class='cakeContents__header'>
+        <div>
           <div>{$r['title']}</div>
           <div>{$r['price']}</div>
         </div>
         <div>
-          <div><img src=$r['url'] alt='cakeImg'></div>
+          <div><img src=$r['url']></div>
         </div>
-        <div class='cakeContents__text'>
+        <div>
           <div>{$r['explanation']}</div>
         </div>
         ";
