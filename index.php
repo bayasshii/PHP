d9a81e62ea69cabbc098aa3bb22be0624a6a58be
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 03:40:59 +0900

    commit

--- index.php
+++ index.php
@@ -7,70 +7,76 @@
   </head>
   <body>
     <div class="wrap">
-      <form action="cakeWrite.php" method="post">
-        <div>
-          <p>お店</p>
-          <input type="text" name="cake_shop">
-        </div>
-        <div>
-          <p>ケーキの名前</p>
-          <input type="text" name="cake_title">
-        </div>
-        <div>
-          <p>特徴</p>
-          <input type="text" name="cake_explanation">
-        </div>
-        <div>
-          <p>値段</p>
-          <input type="text" name="cake_price">
-        </div>
-        <div>
-          <p>ケーキの画像URL</p>
-          <input type="text" name="cake_url">
-        </div>
-        <div>
-          <input type="submit" value="送信！">
-        </div>
-      </form>
-
-      <form action="cakeSearch.php" method="post">
-        <div>
-          <p>ケーキ検索！</p>
-          <input type="text" name="cake_searchWord">
-        </div>
-        <div>
-          <p>価格検索！</p>
-          <input type="text" name="cake_searchMin">
-          <p>〜</p>
-          <input type="text" name="cake_searchMax">
-        </div>
-        <div>
-          <input type="submit" value="検索！">
-        </div>
-      </form>
-
-      <?php
-
-        $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
-
-        $tb = $db->query("SELECT * FROM cakes");
-
-        while ($r = $tb->fetch()){
-          print "
-          <div class='cakeContents__header'>
-            <div>{$r['title']}</div>
-            <div>{$r['price']}</div>
+      <header class="header">
+        <h1>ケーキの検索サイト</h1>
+      </header>
+      
+      <main class="main">
+        <form action="cakeWrite.php" method="post">
+          <div>
+            <p>お店</p>
+            <input type="text" name="cake_shop">
           </div>
           <div>
-            <div><img src={$r['url']} alt='cakeImg'></div>
+            <p>ケーキの名前</p>
+            <input type="text" name="cake_title">
           </div>
-          <div class='cakeContents__text'>
-            <div>{$r['explanation']}</div>
+          <div>
+            <p>特徴</p>
+            <input type="text" name="cake_explanation">
           </div>
-          ";
-        }
+          <div>
+            <p>値段</p>
+            <input type="text" name="cake_price">
+          </div>
+          <div>
+            <p>ケーキの画像URL</p>
+            <input type="text" name="cake_url">
+          </div>
+          <div>
+            <input type="submit" value="送信！">
+          </div>
+        </form>
 
-      ?>
+        <form action="cakeSearch.php" method="post">
+          <div>
+            <p>ケーキ検索！</p>
+            <input type="text" name="cake_searchWord">
+          </div>
+          <div>
+            <p>価格検索！</p>
+            <input type="text" name="cake_searchMin">
+            <p>〜</p>
+            <input type="text" name="cake_searchMax">
+          </div>
+          <div>
+            <input type="submit" value="検索！">
+          </div>
+        </form>
+
+        <?php
+
+          $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
+
+          $tb = $db->query("SELECT * FROM cakes");
+
+          while ($r = $tb->fetch()){
+            print "
+            <div class='cakeContents__header'>
+              <div>{$r['title']}</div>
+              <div>{$r['price']}</div>
+            </div>
+            <div>
+              <div><img src={$r['url']} alt='cakeImg'></div>
+            </div>
+            <div class='cakeContents__text'>
+              <div>{$r['explanation']}</div>
+            </div>
+            ";
+          }
+
+        ?>
+      <main>
     </div>
   </body>
 </html>
