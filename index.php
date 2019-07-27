eb3ba4eef397e489b34ea41f6c0723c1c359a864
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 04:55:48 +0900

    commit

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                 </div>
                 <div class='cakeContents__right'>
                   <div class='cakeContents--url'><img src='https://treeethreee.s3-ap-northeast-1.amazonaws.com/ichigoFTHG4922_TP_V.jpg' alt='cakeImg'></div>
-                  <divclass='cakeContents--price'>{$r['price']}</div>
+                  <divclass='cakeContents--price'>{$r['price']}円</div>
                 </div>
               </div>
             </div>
