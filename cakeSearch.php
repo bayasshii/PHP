e1f558aeeb214ca49f827eef492313d8719b4060
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-07-28 03:05:01 +0900

    Update cakeSearch.php

--- cakeSearch.php
+++ cakeSearch.php
@@ -12,12 +12,14 @@
 
       $db = new PDO("mysql:host=k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e15c99a3pvrcyx7h","v4ve7yaxpa2rgsm2","clzlpibcmu6fn5ks");
 
-      $ps = $db->query("SELECT * FROM cakes WHERE title like '%$cake_searchWord%' OR explain like '%$cake_searchWord%'");
+      print isset($cake_searchWord)?"{$cake_searchWord}』に関連するケーキの検索結果":;
+
+      $ps = $db->query("SELECT * FROM cakes WHERE title like '%$cake_searchWord%'");
 
       while ($r = $ps->fetch()){
         print "{$r['shop']} {$r['title']} {$r['price']} {$r['explanation']} {$r['url']}<hr>";
       }
-    ?>
+      ?>
 
     </body>
   </html>
